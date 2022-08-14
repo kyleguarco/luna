d6ae0ac5cab58f70c3905e83c714bfe95b243cb7
Kyle Guarco <kyleguarco55@gmail.com>
2022-08-13 22:21:30 -0400

    Single-character lexemes (and NotEqual)

--- src/bin/lua.rs
+++ src/bin/lua.rs
@@ -1,51 +1,57 @@
 //! The REPL used to test and run lua code from the command line.
 
-use std::{env::args, process::ExitCode, io::{stdin, BufRead, Write}, str::Chars};
+use std::{
+    env::args,
+    io::{stdin, BufRead, Write},
+    process::ExitCode,
+};
 
 use luna::codegen::{Lexer, Token};
 
 // use luna::codegen::lex::Lexer;
 
 fn main() -> ExitCode {
-	let mut args = args();
+    let mut args = args();
 
-	match args.len() {
-		1 => repl(),
-		2 => {
-			args.next(); // skip program name
-			let spath = args.next().expect("Expected script path");
-			script(&spath)
-		},
-		_ => {
-			eprintln!("Usage: {} [script]", args.next().expect("prog"));
-			ExitCode::FAILURE
-		}
-	}
+    match args.len() {
+        1 => repl(),
+        2 => {
+            args.next(); // skip program name
+            let spath = args.next().expect("Expected script path");
+            script(&spath)
+        }
+        _ => {
+            eprintln!("Usage: {} [script]", args.next().expect("prog"));
+            ExitCode::FAILURE
+        }
+    }
 }
 
 fn script(path: &str) -> ExitCode {
-	println!("{path}");
+    println!("{path}");
 
-	ExitCode::SUCCESS
+    ExitCode::SUCCESS
 }
 
 fn repl() -> ExitCode {
-	let mut io = stdin().lock();
-	let mut line = String::new();
+    let mut io = stdin().lock();
+    let mut line = String::new();
 
-	loop {
-		line.clear();
+    loop {
+        line.clear();
 
-		print!(">> ");
-		std::io::stdout().flush().unwrap();
+        print!(">> ");
+        std::io::stdout().flush().unwrap();
 
-		io.read_line(&mut line).expect("Couldn't read REPL line.");
+        io.read_line(&mut line).expect("Couldn't read REPL line.");
 
-		if line.starts_with("exit") { break }
+        if line.starts_with("exit") {
+            break;
+        }
 
-		let lexer: Lexer = line.chars().peekable().into();
-		println!("{:?}", lexer.collect::<Vec<Token>>());
-	}
+        let lexer = Lexer::new(&line);
+        println!("{:?}", lexer.collect::<Vec<Token>>());
+    }
 
-	ExitCode::SUCCESS
+    ExitCode::SUCCESS
 }
--- src/codegen.rs
+++ src/codegen.rs
@@ -1,8 +1,8 @@
-use core::{ops::Deref, str::Chars, iter::Peekable};
+use core::ops::Deref;
 
 #[rustfmt::skip]
 #[derive(Clone, Debug)]
-enum TokenType {
+pub enum TokenType {
 	// Keywords
 	And, Break, Do, Else, ElseIf, End, False, For, Function,
 	Goto, If, In, Local, Nil, Not, Or, Repeat, Return, Then,
@@ -14,52 +14,131 @@ enum TokenType {
 	Dot, Colon, Comma, Semicolon,
 	// Terminal Symbols (greater than one)
 	ShiftLeft, ShiftRight, DoubleSlash, Equality, NotEqual,
-	LessEqual, GreaterEqual, DoubleColon, DoubleDots, TripleDots
+	LessEqual, GreaterEqual, DoubleColon, DoubleDots, TripleDots,
+    // Literals
+    Identifier, String, Number, Float
 }
 
 #[derive(Clone, Debug)]
-pub struct Token {
+pub struct Token<'src> {
     ttype: TokenType,
+    lexeme: &'src str,
     line: usize,
     col: usize,
 }
 
+impl<'src> Token<'src> {
+    pub fn token_type(&self) -> &TokenType {
+        &self.ttype
+    }
+
+    pub fn position(&self) -> (usize, usize) {
+        (self.line, self.col)
+    }
+
+    pub fn lexeme(&self) -> &'src str {
+        self.lexeme
+    }
+}
+
 pub struct Lexer<'src> {
-    source: Peekable<Chars<'src>>,
+    source: &'src str,
+    last: usize,
     position: usize,
     line: usize,
     col: usize,
 }
 
-impl<'src, I> From<I> for Lexer<'src>
-where
-    Peekable<Chars<'src>>: From<I>,
-{
-    fn from(source: I) -> Self {
-        Lexer {
-            source: source.into(),
+impl<'src> Lexer<'src> {
+    pub fn new<T: Deref<Target = str>>(source: &'src T) -> Self {
+        Self {
+            source: source.as_ref(),
+            last: 0,
             position: 0,
             line: 1,
-            col: 1,
+            col: 0,
         }
+    }
+
+    fn is_end(&self) -> bool {
+        self.position >= self.source.len()
+    }
+
+    fn peek(&self, peek: usize) -> &'src str {
+        let slice = if self.position + peek >= self.source.len() {
+            self.position..self.source.len() - 1
+        } else {
+            self.position..self.position + peek
+        };
+        self.source.get(slice).unwrap()
+    }
+
+    fn tokenize(&mut self, ttype: TokenType) -> Token<'src> {
+        let token = Token {
+            ttype,
+            lexeme: self.source.get(self.last..self.position).unwrap(),
+            line: self.line,
+            col: self.col,
+        };
+        self.last = self.position;
+        token
+    }
+
+    fn advance(&mut self, seek: usize) -> &'src str {
+        let slice = self.peek(seek);
+        self.position += seek;
+        if slice.contains("\n") {
+            self.line += 1;
+            self.col = 1;
+        } else {
+            self.col += seek;
+        }
+        slice
     }
 }
 
-impl Lexer<'_> {
-	fn new_token(&self, ttype: TokenType) -> Token {
-		Token { ttype, line: self.line, col: self.col }
-	}
-}
-
 impl<'src> Iterator for Lexer<'src> {
-    type Item = Token;
+    type Item = Token<'src>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.position == 10 {
-			return None
-		}
+        if self.is_end() {
+            return None;
+        }
 
-		self.position += 1;
-		Some(self.new_token(TokenType::And))
+        //while self.peek(1) != " " { self.position += 1; }
+
+        match self.advance(1) {
+            // Terminal Symbols
+            "+" => Some(self.tokenize(TokenType::Plus)),
+            "-" => Some(self.tokenize(TokenType::Minus)),
+            "*" => Some(self.tokenize(TokenType::Star)),
+            "/" => Some(self.tokenize(TokenType::Slash)),
+            "%" => Some(self.tokenize(TokenType::Percent)),
+            "^" => Some(self.tokenize(TokenType::Caret)),
+            "#" => Some(self.tokenize(TokenType::Pound)),
+            "&" => Some(self.tokenize(TokenType::Amphersand)),
+            "~" => match self.peek(1) {
+                "=" => {
+                    self.advance(1);
+                    Some(self.tokenize(TokenType::NotEqual))
+                }
+                _ => Some(self.tokenize(TokenType::Tilde)),
+            },
+            "|" => Some(self.tokenize(TokenType::Pipe)),
+            "<" => Some(self.tokenize(TokenType::LessThan)),
+            ">" => Some(self.tokenize(TokenType::GreaterThan)),
+            "=" => Some(self.tokenize(TokenType::Equal)),
+            "(" => Some(self.tokenize(TokenType::LeftParen)),
+            ")" => Some(self.tokenize(TokenType::RightParen)),
+            "{" => Some(self.tokenize(TokenType::LeftBrace)),
+            "}" => Some(self.tokenize(TokenType::RightBrace)),
+            "[" => Some(self.tokenize(TokenType::LeftBracket)),
+            "]" => Some(self.tokenize(TokenType::RightBracket)),
+            "." => Some(self.tokenize(TokenType::Dot)),
+            ":" => Some(self.tokenize(TokenType::Colon)),
+            "," => Some(self.tokenize(TokenType::Comma)),
+            ";" => Some(self.tokenize(TokenType::Semicolon)),
+            _ => None,
+        }
     }
 }
